394464f9a80fb4e95dbac5d4aecec77264947e44
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2024-11-27 13:54:57 +0100

    Annotaitons

---
+++ DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry
+    {
+        //[Key]
+        public int Id { get; set; }
+
+        [Required]
+        public required string? Title { get; set; } =string.Empty;
+
+        [Required]
+        public string Content { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        //datetime.now == data, ktora jest teraz podczas tworzenia
+    }
+}
