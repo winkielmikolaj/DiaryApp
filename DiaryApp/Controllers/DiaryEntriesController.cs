21d484bad61b7f21dcabb0c07229e43f048c0265
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2024-11-28 11:26:36 +0100

    Adding controller and view to it

---
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiaryApp.Controllers
+{
+    public class DiaryEntriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
