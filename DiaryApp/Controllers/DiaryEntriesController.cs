429cc3d18bfd6722f3b4b648fd48bb8166aeaaca
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2024-11-30 13:07:30 +0100

    Edit operation

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -63,5 +63,66 @@ namespace DiaryApp.Controllers
 
             return View(obj);
         }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            DiaryEntry diaryEntry = _db.DiaryEntries.FirstOrDefault(x => x.Id == id);
+
+            if (diaryEntry == null)
+            {
+                return NotFound();
+            }
+
+            return View(diaryEntry);
+        }
+        [HttpPost]
+        public IActionResult Edit(DiaryEntry obj)
+        {
+            //server side validation
+            if (obj != null)
+            {
+                if (obj.Title.Length < 3)
+                {
+                    ModelState.AddModelError("Title", "Title too short!");
+                }
+                else if (obj.Title.Length > 15)
+                {
+                    ModelState.AddModelError("Title", "Title too long!");
+                }
+
+                if (obj.Content.Length < 15)
+                {
+                    ModelState.AddModelError("Content", "Description is too short!");
+                }
+                else if (obj.Content.Length > 500)
+                {
+                    ModelState.AddModelError("Content", "Description is too long!");
+                }
+            }
+
+
+            //another server side validation
+            if (ModelState.IsValid)
+            {
+                _db.DiaryEntries.Update(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
     }
 }
